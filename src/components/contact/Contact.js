120bb515876f82ca57e162d83ceb39d880d063a1
Gegardus <stgegard@gmail.com>
2022-06-30 21:04:03 +0400

    Install emailjs

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,11 +1,15 @@
 import React, { useRef } from 'react';
+import emailjs from 'emailjs-com';
+import EventTracker from '../events/EventTracker';
 import { MdOutlineMail } from 'react-icons/md';
 import './contact.css';
 
 const Contact = () => {
+  const eventTracker = EventTracker('Contact me');
   const form = useRef();
   const sendEmail = (e) => {
     e.preventDefault();
+    emailjs.sendForm('service_451', 'template_pi4dfqm', form.current, 'RZgtb160BfyfW9KwR');
     e.target.reset();
   };
   return (
@@ -18,7 +22,11 @@ const Contact = () => {
             <MdOutlineMail className="contact__option-icon" />
             <h4>Email</h4>
             <h5>Vahan Khachvankian</h5>
-            <a href="mailto:stgegard@gmail.com" target="_blank" rel="noreferrer">
+            <a
+              href="mailto:stgegard@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+              onClick={() => eventTracker('Direct Email')}>
               Send a message
             </a>
           </article>
@@ -29,6 +37,12 @@ const Contact = () => {
           <input type="text" name="name" placeholder="Your Full Name" required />
           <input type="email" name="email" placeholder="Your Email" required />
           <textarea name="message" rows="7" placeholder="Your Message" required />
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={() => eventTracker('Text message')}>
+            Send Message
+          </button>
         </form>
       </div>
     </section>
